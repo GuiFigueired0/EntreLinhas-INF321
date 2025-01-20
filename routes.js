5cd3efd55791b5da8b17c7c86d23a2afa6781c22
Guilherme Figueiredo <guilhermefigueiredo341@gmail.com>
2025-01-20 16:12:56 -0300

    db_playground

--- routes.js
+++ routes.js
@@ -4,17 +4,29 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
 const contatoController = require('./src/controllers/contatoController');
+const bookController = require('./src/controllers/bookController');
+const playgroundController = require('./src/controllers/playgroundController');
 
 const { loginRequired } = require('./src/middlewares/middleware');
 
 // Rotas da home
 route.get('/', homeController.index);
 
+// Rota do DB Playground
+route.get('/db_playground', playgroundController.index);
+
 // Rotas de login
 route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
+
+// Rotas de Livro
+route.post('/books', bookController.create); 
+route.get('/books/genre/:genre', bookController.findByGenre); 
+route.get('/books/:id', bookController.findById); 
+route.get('/books/search/:title', bookController.searchByTitle); 
+route.get('/books/:id/similar', bookController.findSimilarBooks);
 
 // Rotas de contato
 route.get('/contato/index', loginRequired, contatoController.index);
