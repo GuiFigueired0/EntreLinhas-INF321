ac1e63279a676ff3a9ab5e95514f56ecbec3688b
andre-n-alcantara <andre.n.alcantara@ufv.br>
2025-01-21 22:23:07 -0300

    gallery asset

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,4 +4,21 @@ document.addEventListener('DOMContentLoaded', function () {
       e.preventDefault();
       document.body.classList.toggle('sb-expanded');
   });
+  const gridViewSelect = document.getElementById('gridViewSelect');
+  const listViewSelect = document.getElementById('listViewSelect');
+
+  if (gridViewSelect && listViewSelect) {
+    gridViewSelect.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.getElementById('listView').classList.add('d-none');
+      document.getElementById('gridView').classList.remove('d-none');
+    });
+
+    listViewSelect.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.getElementById('gridView').classList.add('d-none');
+      document.getElementById('listView').classList.remove('d-none');
+    });
+  }
 });
+
