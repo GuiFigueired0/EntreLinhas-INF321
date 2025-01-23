b9755b506515dc93a0144ff96aa397088a5d7a96
andre-n-alcantara <andre.n.alcantara@ufv.br>
2025-01-23 15:56:32 -0300

    mock completo (desorganizado)

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const resizeBtn = document.querySelector('[data-resize-btn]');
-  resizeBtn.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.body.classList.toggle('sb-expanded');
-  });
+  const sidebarBtn = document.getElementById('sidebarBtn');
+  if (document.body.classList.contains('sb-toggle') && sidebarBtn) {
+    sidebarBtn.classList.remove('d-none');
+    sidebarBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.body.classList.toggle('sb-expanded');
+    });
+  }
   const gridViewSelect = document.getElementById('gridViewSelect');
   const listViewSelect = document.getElementById('listViewSelect');
 
@@ -62,14 +65,23 @@ document.addEventListener('DOMContentLoaded', function () {
           this.classList.add('active');
 
           // Hide all tab contents
-          tabDivs.forEach(tab => tab.style.display = 'none');
+          tabDivs.forEach(tab => tab.classList.add('d-none'));
 
           // Get the target classes from the data attribute
           const targetSelector = this.getAttribute('target-selector');
 
           // Display the elements that match the target selector
           const elements = document.querySelectorAll(targetSelector);
-          elements.forEach(element => element.style.display = 'block');
+          elements.forEach(element => element.classList.remove('d-none'));
+
+            // Check if the clicked link has the 'tab-info' class
+            if (this.classList.contains('sb-expanded')) {
+            // Add 'tab-info' class to the body element
+                document.body.classList.add('sb-expanded');
+            } else {
+                // Remove 'tab-info' class from the body element
+                document.body.classList.remove('sb-expanded');
+            }
       });
   });
 
